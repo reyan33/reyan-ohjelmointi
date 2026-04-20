18abc4d37176c52da13d3b64fed51c9a992cb61f
Reyan <reyanyenate@gmail.com>
2026-04-20 09:44:48 +0300

    Lisätty lomakkeen lähetys ja konsolitulostus

--- portfolio/keskustelu.js
+++ portfolio/keskustelu.js
@@ -3,7 +3,7 @@ import { saveComments, commentsData } from "./utils.js";
 const checkBox = document.getElementById("check")
 const form = document.getElementById("form");
  
-form.addEventListener("submit", (e)=>{
+/* form.addEventListener("submit", (e)=>{
   if(checkBox.checked == true){
 
     e.preventDefault();
@@ -18,7 +18,7 @@ form.addEventListener("submit", (e)=>{
     saveComments();
     window.location.href = "./aiemat_kommentit.html";
   }
-}); 
+}); */
 
 const dialog = document.getElementById("commentDialog");
 const openBtn = document.getElementById("openDialog");
@@ -27,3 +27,21 @@ const closeBtn = document.getElementById("closeDialog");
 openBtn.addEventListener("click", () => dialog.showModal());
 closeBtn.addEventListener("click", () => dialog.close());
 
+function send(e) {
+    e.preventDefault();
+
+    let birthday = document.getElementById("paiva").value;
+    let email = document.getElementById("email").value;
+    let type = document.getElementById("type").value;
+    let usage = document.getElementById("check").checked;
+    let body = document.getElementById("message").value;
+
+    console.log("birthday:", birthday);
+    console.log("email:", email);
+    console.log("type:", type);
+    console.log("usage:", usage);
+    console.log("body:", body);
+
+    dialog.close();
+}
+window.send = send;
